a7f2a2632e4cfda7fd6d3cb5f081a487c3e6fe58
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-03-25 14:12:00 +0530

    - Copy to s3 using tf.gfile

--- tf-samples/object-detection/preprocess/download.py
+++ tf-samples/object-detection/preprocess/download.py
@@ -1,7 +1,9 @@
 import six.moves.urllib as urllib
 import os
+import tensorfow as tf
 
 OUTPUT_DIR = os.getenv('OUT_DIR', None)
 opener = urllib.request.URLopener()
-opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", OUTPUT_DIR + "/annotations.tar.gz")
+opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", "./annotations.tar.gz")
+tf.gfile.Copy("./annotations.tar.gz", OUTPUT_DIR + "/annotations.tar.gz")
 print("Saved in", OUTPUT_DIR)
