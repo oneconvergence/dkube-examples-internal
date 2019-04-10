95a8b47f7df5e894021a889bacec56933db19d37
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-04-10 22:19:55 +0530

    - Object detection example (Oxford pets dataset)

---
+++ tf-samples/objdet-pets/process.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATA_DIR="${DATUMS_PATH}/${DATASET_NAME}"
+MODEL_DIR="${MODEL_PATH}/${MODEL_NAME}"
+
+#Set datset path in pipeline.config file
+sed -i "s|DATA_PATH|"${DATA_DIR}"|g" ./pipeline.config
+
+EXTRACT_PATH="/tmp/object-detection"
+mkdir -p $EXTRACT_PATH
+echo "Extract path : $EXTRACT_PATH"
+
+#Extract model
+for file in $MODEL_DIR/*; do
+	filename=$(basename -- "$file")
+	extension="${filename##*.}"
+        echo "Model file format : $extension"
+	if [[ $extension == "gz" ]]; then
+        	echo "Extracting model"
+                tar -xvf $file -C $EXTRACT_PATH
+	elif [[ $extension == "zip" ]]; then
+        	echo "Extracting model"
+                unzip $file -d $EXTRACT_PATH
+	else
+        	echo "Unsupported format"
+		exit 1
+	fi
+	done
+
+#Set the model path in pipeline.config file to the extracted path
+sed -i "s|MODEL_PATH|"${EXTRACT_PATH}"|g" ./pipeline.config
