04022398ea32b96bc115dc1806d4e07c1e96fba1
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-07-17 15:38:50 +0530

    - export

--- keras/classification/mnist/classifier/model.py
+++ keras/classification/mnist/classifier/model.py
@@ -20,12 +20,18 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
+from keras.models import load_model
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model.signature_def_utils import predict_signature_def
+from tensorflow.python.saved_model import tag_constants
+
 now = datetime.datetime.now
 
 batch_size = int(os.getenv('TF_BATCH_SIZE', 128))
 epochs = int(os.getenv('TF_EPOCHS', 5))
 num_classes = 5
 
+out_dir = os.getenv('OUT_DIR', None)
 save_dir = "/tmp"
 model_name = 'keras_mnist_trained_model.h5'
 
@@ -80,6 +86,28 @@ def train_model(model, train, test, num_classes):
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
+    
+def export_h5_to_pb(path_to_h5, export_path):
+
+    # Set the learning phase to Test since the model is already trained.
+    K.set_learning_phase(0)
+
+    # Load the Keras model
+    keras_model = load_model(path_to_h5)
+
+    # Build the Protocol Buffer SavedModel at 'export_path'
+    builder = saved_model_builder.SavedModelBuilder(export_path)
+
+    # Create prediction signature to be used by TensorFlow Serving Predict API
+    signature = predict_signature_def(inputs={"images": keras_model.input},
+                                      outputs={"scores": keras_model.output})
+
+    with K.get_session() as sess:
+        # Save the meta graph and the variables
+        builder.add_meta_graph_and_variables(sess=sess, tags=[tag_constants.SERVING],
+                                         signature_def_map={"predict": signature})
+
+    builder.save()
 
 
 # the data, split between train and test sets
@@ -133,5 +161,7 @@ for l in feature_layers:
 train_model(model,
             (x_train_gte5, y_train_gte5),
             (x_test_gte5, y_test_gte5), num_classes)
+            
+export_h5_to_pb(os.path.join(save_dir, model_name), os.path.join(out_dir, "1"))
             
 
