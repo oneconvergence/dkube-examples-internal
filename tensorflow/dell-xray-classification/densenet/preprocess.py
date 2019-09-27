7d199b76dc7a7a88911ce7c6e61d6e0d54af299c
maheshtambe <mahesh.tambe@oneconvergence.com>
2019-09-27 16:57:08 +0530

    preprocess code

--- tensorflow/dell-xray-classification/densenet/preprocess.py
+++ tensorflow/dell-xray-classification/densenet/preprocess.py
@@ -29,14 +29,14 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-DATA_FOLDER = '/tmp/data/'
-INDICES_FILE = './Data_Entry_2017.csv'
-MIN_CASES = 1000
-# zip file of preprocessed data
-INPUT_DATA_DIR = "./data"
+DATA_FOLDER = '/tmp/data/images'
+# INDICES_FILE = './Data_Entry_2017.csv'
+# MIN_CASES = 1000
+# # zip file of preprocessed data
+# INPUT_DATA_DIR = "./data"
 OUTPUT_ZIP_FILENAME = "data.zip"
-TRAINING_CSV_FILE = "dataset_train.csv"
-VALID_CSV_FILE = "dataset_validation.csv"
+# TRAINING_CSV_FILE = "dataset_train.csv"
+# VALID_CSV_FILE = "dataset_validation.csv"
 
 
 class DataPreparation:
@@ -46,31 +46,32 @@ class DataPreparation:
         self.input_dir = input_dir
         self.output_dir = output_dir
         # store images: path key-value pair
-        self.metadata = None
+        #self.metadata = None
 
     def run(self):
         """Run Data Preparation steps."""
         # print("input_dir: {}\nOutput_dir: {}".format(
         #     self.input_dir, self.output_dir))
 
+        os.makedirs(DATA_FOLDER)
         # 1. Untar all tar files and save all images to DATA_FOLDER
         self.extract_tarfiles()
         # 2. Load the metadata from indices CSV file
-        metadata = self.load_metadata()
-        # 3. Preprocessing of the metadata DataFrames
-        metadata, labels = self.preprocess_metadata(metadata)
-        # 4. Creates a train/test stratification of the dataset
-        train, valid = self.stratify_train_test_split(metadata)
+        # metadata = self.load_metadata()
+        # # 3. Preprocessing of the metadata DataFrames
+        # metadata, labels = self.preprocess_metadata(metadata)
+        # # 4. Creates a train/test stratification of the dataset
+        # train, valid = self.stratify_train_test_split(metadata)
 
-        # 5. Perform Data Augmentation
-        # # self.data_augmentation()
+        # # 5. Perform Data Augmentation
+        # # # self.data_augmentation()
 
-        # 6. create dataset and copy all images into images dir
-        self.create_dataset(train, valid)
+        # # 6. create dataset and copy all images into images dir
+        # self.create_dataset(train, valid)
         # 7. Creating dataset zipfile
         self.zip()
         # 8. Remove dataset
-        self.remove_directory()
+        #self.remove_directory()
 
     def extract_tarfiles(self):
         """
@@ -257,7 +258,7 @@ class DataPreparation:
         print("Creating dataset zip file. ")
         with zipfile.ZipFile(output_zip_filename, 'w') as zip_ref:
             # writing each file one by one
-            for root, dirs, files in os.walk(INPUT_DATA_DIR):
+            for root, dirs, files in os.walk("/tmp/data"):
                 for file in files:
                     zip_ref.write(os.path.join(root, file))
         print("Dataset is available here: %s" % (output_zip_filename))
