7331f3bbb4c0702d383e5fbc7d4174c57b472047
Rajendra Singh <rajendra.singh@oneconvergence.com>
2020-07-19 14:26:14 -0500

    fixed np_utils.py

--- keras/classification/fashion-mnist/train.py
+++ keras/classification/fashion-mnist/train.py
@@ -3,7 +3,7 @@ import os
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
-from tensorflow.keras.utils import np_utils
+from tensorflow.keras import utils
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras import metrics
@@ -47,8 +47,8 @@ X_train = X_train.reshape(X_train.shape[0], img_width, img_height, 1)
 X_test = X_test.reshape(X_test.shape[0], img_width, img_height, 1)
 
 # one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+y_train = utils.to_categorical(y_train)
+y_test = utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
 sgd = SGD(
