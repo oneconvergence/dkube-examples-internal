2f8b8f2fd1f7a04f1bffa96cd9a50aa8c5e81fd6
dkube <dkube@oneconvergence.com>
2020-08-19 12:22:07 -0400

    class file for kfserving

--- pytorch/classification/mnist/digits/classifier/program/class_file/Net.py
+++ pytorch/classification/mnist/digits/classifier/program/class_file/Net.py
@@ -1,14 +1,6 @@
-from __future__ import print_function
-import argparse
-import os, shutil
-import json
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
-from torch.utils.tensorboard import SummaryWriter
 
 class Net(nn.Module):
     def __init__(self):
