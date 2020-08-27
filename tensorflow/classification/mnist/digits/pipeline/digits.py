731924881b0e15e11f5f672b66177bc106b1df2d
mak-454 <ahmed.khan@oneconvergence.com>
2020-08-27 19:01:48 +0000

    made the project, dataset and model as mandatory inputs

--- tensorflow/classification/mnist/digits/pipeline/digits.py
+++ tensorflow/classification/mnist/digits/pipeline/digits.py
@@ -16,6 +16,12 @@ dkube_viewer_op             = components.load_component_from_file('../components
 def d3pipeline(
     #Dkube authentication token
     auth_token,
+    #Name of the project in dkube
+    training_program,
+    #Dataset to train on
+    training_dataset,
+    #Output model 
+    training_output_model,
     #By default 'default' is used as the job group for runs
     job_group = 'default',
     #Framework. One of tensorflow, pytorch, sklearn
@@ -26,19 +32,13 @@ def d3pipeline(
     #Or any other custom image name can be supplied.
     #For custom private images, please input username/password
     training_container=json.dumps({'image':'docker.io/ocdr/d3-datascience-tf-cpu:v1.14', 'username':'', 'password': ''}),
-    #Name of the workspace in dkube. Update accordingly if different name is used while creating a workspace in dkube.
-    training_program="mnist",
     #Script to run inside the training container
     training_script="python model.py",
     tuning = '{ "studyName": "mnist-dkube-tuning", "owner": "crd", "optimizationtype": "maximize", "objectivevaluename": "train_accuracy_1", "optimizationgoal": 0.99, "requestcount": 1, "metricsnames": [ "train_accuracy_1" ], "parameterconfigs": [ { "name": "--learning_rate", "parametertype": "double", "feasible": { "min": "0.01", "max": "0.05" } }, { "name": "--batch_size", "parametertype": "int", "feasible": { "min": "100", "max": "200" } }, { "name": "--num_epochs", "parametertype": "int", "feasible": { "min": "1", "max": "10" } } ] }',
-    #Input datasets for training. Update accordingly if different name is used while creating dataset in dkube.
-    training_datasets=json.dumps(["mnist"]),
-    #Input dataset mount paths
-    training_input_dataset_mounts=json.dumps(["/opt/dkube/input"]),
-    #Output models for training.
-    training_outputs=json.dumps(["mnist"]),
+    #Input dataset mount path
+    training_input_dataset_mount="/opt/dkube/input",
     #Output dataset mount paths
-    training_output_mounts=json.dumps(["/opt/dkube/output"]),
+    training_output_mount="/opt/dkube/output",
     #Request gpus as needed. Val 0 means no gpu, then training_container=docker.io/ocdr/dkube-datascience-tf-cpu:v1.12
     training_gpus=0,
     #Any envs to be passed to the training program
@@ -56,9 +56,9 @@ def d3pipeline(
 
     train       = dkube_training_op(auth_token, training_container,
                                     program=training_program, run_script=training_script,
-                                    datasets=training_datasets, outputs=training_outputs,
-                                    input_dataset_mounts=training_input_dataset_mounts,
-                                    output_mounts=training_output_mounts,
+                                    datasets=json.dumps([str(training_dataset)]), outputs=json.dumps([str(training_output_model)]),
+                                    input_dataset_mounts=json.dumps([str(training_input_dataset_mount)]),
+                                    output_mounts=json.dumps([str(training_output_mount)]),
                                     ngpus=training_gpus, envs=training_envs,
                                     tuning=tuning, job_group=job_group,
                                     framework=framework, version=version)
