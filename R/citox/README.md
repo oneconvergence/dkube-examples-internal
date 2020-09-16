64477b3c847123dc720238266a39c1eb66e1cb99
mak-454 <ahmed.khan@oneconvergence.com>
2020-09-16 10:36:16 +0000

    sample demo R model with kfserving and dkube

---
+++ R/citox/README.md
@@ -0,0 +1,7 @@
+# Edit name in r-inf.yaml
+
+# kubectl create -f r-inf.yaml
+
+# export TOKEN=<dkube-token>
+
+# curl -k -s -d @data.json -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -X POST https://35.222.39.222:32222/dkube/inf/v1/models/<modelname>:predict
---
+++ R/citox/mnist.R
@@ -0,0 +1,48 @@
+library(methods)
+library(randomForest)
+
+
+dinucs <- function(nucs = c('A','T','C','G'), sugars = c('L','D')){
+  
+  states.di <- apply(expand.grid(nucs, nucs,'_',sugars,sugars), 1, 
+                     function(x) paste(x,collapse = ''))
+  sort(states.di)
+  
+}
+
+split2di <- function(x) {  
+  
+  alldi <- dinucs()
+  
+  a <- paste(substring(x[1],1:(nchar(x[1])-1),2:(nchar(x[1]))),
+             substring(x[2],1:(nchar(x[2])-1),2:(nchar(x[2]))),sep='_')
+  a <- factor(a,levels = alldi)
+  table(a)
+}
+
+
+predict.mnist <- function(mnist,newdata=list()) {
+  rf_model =mnist$model 
+  i <- c(5, 6)
+  newdata[ , i] <- apply(newdata[ , i], 2,function(x) as.numeric(as.character(x)))
+  feature_tab  <- newdata
+  model_features <- t(apply(feature_tab[,1:2], 1, split2di))
+  model_features <- as.data.frame(model_features)
+  model_features$dg = unlist(feature_tab[,5])
+  res <- stats::predict(rf_model,model_features)
+  data.frame(res)
+}
+send_feedback.mnist <- function(mnist,request=list(),reward=1,truth=list()) {
+}
+
+new_rf <- function(filename) {
+  model <- readRDS(filename)
+  structure(list(model=model), class = "mnist")
+
+}
+
+initialise_seldon <- function(filename, params) {
+  #new_rf("/microservice/mnist.Rds")
+  new_rf(filename)
+}
+                  
