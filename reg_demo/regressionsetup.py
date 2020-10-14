a612736962d8f512dd44861c4b89b431882318b4
riteshkarvaloc <57743573+riteshkarvaloc@users.noreply.github.com>
2020-10-14 11:52:41 +0530

    fixes

--- reg_demo/regressionsetup.py
+++ reg_demo/regressionsetup.py
@@ -14,57 +14,63 @@ authToken = args.authtoken
 user = args.user
 
 api = DkubeApi(dkubeURL=dkubeURL, authToken=authToken)
-
-project = DkubeProject(user, name='regression')
-project.update_project_source(source='github')
-project.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo/reg_demo', branch='reg-demo')
 try:
+    res = api.get_project(user, 'regression')
+    print("Datum already exists")
+except:
+    project = DkubeProject(user, name='regression')
+    project.update_project_source(source='github')
+    project.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-2-1-3-1/reg_demo', branch='reg-demo-2-1-3-1')
     api.create_project(project)
-except:
-    print("Datum already exists")
+
     
-dataset = DkubeDataset(user, name='clinical')
-dataset.update_dataset_source(source='github')
-dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/clinical', branch='reg-demo-data')
 try:
-    api.create_dataset(dataset)
-except:
+    res = api.get_dataset(user, 'clinical')
     print("Datum already exists")
+except:
+    dataset = DkubeDataset(user, name='clinical')
+    dataset.update_dataset_source(source='github')
+    dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/clinical', branch='reg-demo-data')
+    api.create_dataset(dataset)
+
+try:
+    res = api.get_dataset(user, 'images')
+    print("Datum already exists")
+except:
+    dataset = DkubeDataset(user, name='images')
+    dataset.update_dataset_source(source='github')
+    dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/image_data', branch='reg-demo-data')
+    api.create_dataset(dataset)
     
-dataset = DkubeDataset(user, name='images')
-dataset.update_dataset_source(source='github')
-dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/image_data', branch='reg-demo-data')
 try:
-    api.create_dataset(dataset)
-except:
+    res = api.get_dataset(user, 'rna')
     print("Datum already exists")
-    
-dataset = DkubeDataset(user, name='rna')
-dataset.update_dataset_source(source='github')
-dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/rna', branch='reg-demo-data')
-try:
-    api.create_dataset(dataset)
 except:
-    print("Datum already exists")
+    dataset = DkubeDataset(user, name='rna')
+    dataset.update_dataset_source(source='github')
+    dataset.update_github_details('https://github.com/oneconvergence/dkube-examples/tree/reg-demo-data/reg_demo_data/rna', branch='reg-demo-data')
+    api.create_dataset(dataset)
+
     
 dvs_datasets = ['clinical-preprocessed', 'clinical-train', 'clinical-test', 'clinical-val',
                 'images-preprocessed', 'images-train', 'images-test', 'images-val',
                 'rna-train', 'rna-test', 'rna-val']
 
 for each_dataset in dvs_datasets:
-    dataset = DkubeDataset(user, name=each_dataset)
-    dataset.update_dataset_source(source='dvs')
     try:
-        api.create_dataset(dataset)
-    except:
+        res = api.get_dataset(user, name=each_dataset)
         print("Datum already exists")
-        
-model = DkubeModel(user, name='regression-model')
-model.update_model_source(source='dvs')
+    except:
+        dataset = DkubeDataset(user, name=each_dataset)
+        dataset.update_dataset_source(source='dvs')
+        api.create_dataset(dataset)
+ 
 try:
-    api.create_model(model)
-except:
+    res = api.get_model(user, 'regression-model')
     print("Datum already exists")
-
+except:
+    model = DkubeModel(user, name='regression-model')
+    model.update_model_source(source='dvs')
+    api.create_model(model)
 print("Finishing Dataset creation")
 time.sleep(60)
