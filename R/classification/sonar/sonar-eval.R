eb8004dd75bdac876379203991bec65988696e5c
pallavi-pannu-oc <pallavi.pannu@oneconvergence.com>
2020-10-27 10:25:58 +0000

    r-example

--- R/classification/sonar/sonar-eval.R
+++ R/classification/sonar/sonar-eval.R
@@ -1,7 +1,10 @@
+  
 library(caret)
 library(mlbench)
 library(randomForest)
 library(doMC)
+library(httr)
+library(hash)
 
 IN_DIR <- "/opt/dkube/model/"
 
@@ -16,4 +19,29 @@ model <- readRDS(paste(IN_DIR, "model.rds"))
 print(model)
 # make a predictions on "new data" using the final model
 final_predictions <- predict(model, validation[,1:60])
-confusionMatrix(final_predictions, validation$Class)
+result=confusionMatrix(final_predictions, validation$Class)
+##### Retrieving the metrics ######
+precision <- result$byClass['Pos Pred Value']    
+recall <- result$byClass['Sensitivity']
+accuracy <- result$byClass['Balanced Accuracy']
+
+#### Metric function #####
+log_metrics<-function(key, value){
+url <-"http://dkube-exporter.dkube:9401/mlflow-exporter"
+
+train_metrics<-hash()
+train_metrics[['mode']]<-"train"
+train_metrics[['key']]<-key
+train_metrics[['value']]<-value
+train_metrics[['epoch']]<-1
+train_metrics[['step']]<-1
+train_metrics[['jobid']]<-os.getenv('DKUBE_JOB_ID')
+train_metrics[['run_id']]<-os.getenv('DKUBE_JOB_UUID')
+train_metrics[['username']]<-os.getenv('DKUBE_USER_LOGIN_NAME')
+POST(url,body = train_metrics,encode="json")}
+
+##### Logging the metrics #######
+log_metrics('Precision',precision)
+log_metrics('Recall',recall)
+log_metrics('Accuracy',accuracy)
+
