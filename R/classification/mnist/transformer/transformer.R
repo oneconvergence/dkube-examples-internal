05ee84c149ff24ebd5b1c849cb8b40dbe53de61b
pallavi-pannu-oc <pallavi.pannu@oneconvergence.com>
2020-11-17 05:14:27 +0000

    r-transformer

--- R/classification/mnist/transformer/transformer.R
+++ R/classification/mnist/transformer/transformer.R
@@ -6,13 +6,30 @@ library(optparse)
 library(urltools)
 library(stringi)
 
+library(hash)
+library(float)
+library(utf8)
+library(OpenImage)
 
 
 preprocess <- function(json_data) {
-result <- fromJSON(json_data)
-df<-data.frame(result$inputs)
-return df 
+  json_data$instances<-NULL
+  data=json_data$signatures$inputs[[1]][[3]]
+  json_data=utf8_encode(data)
+  image <- readImage("image.png")
+  image <- resizeImage(image, width = 28, height = 28, method = 'nearest')
+  image <- array(image[1:28,1:28], dim = c(1,1,28,28))
+  image <- dbl(image)
+  instances<-list(image)
+  token <- json_data["token"]
+  payload<-hash()
+  payload[["instances"]]=instances
+  payload[["token"]]=token
+  payload
 }
+
+
+
 
 postprocess <- function(df){
 df
