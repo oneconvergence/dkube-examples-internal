076bd6b7ccedf932d251bdafe8806db9834257ee
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-11-18 09:50:50 +0000

    sdk  read write

--- tensorflow/classification/mnist-fs/digits/classifier/program/features.py
+++ tensorflow/classification/mnist-fs/digits/classifier/program/features.py
@@ -2,8 +2,8 @@ import numpy as np
 import struct
 import os
 import pandas as pd
-
 from dkube.sdk import *
+import argparse
 
 inp_path = '/opt/dkube/input/'
 out_path = '/opt/dkube/output/'
@@ -29,12 +29,15 @@ def read_idx(dataset = "training", path = "../data"):
 
 if __name__ == "__main__":
 
-
-    dkubeURL = 'https://172.16.146.128:32222'
-    authToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0YmNkZjBmZWJmNDRiOGRhZGQxZWIyOGM2MjhkYWYxIn0.eyJ1c2VybmFtZSI6Im9zbSIsInJvbGUiOiJkYXRhc2NpZW50aXN0LG1sZSxwZSxvcGVyYXRvciIsImV4cCI6NDg0NTY0NDMyMywiaWF0IjoxNjA1NjQ0MzIzLCJpc3MiOiJES3ViZSJ9.xMd-OpVe4-RdVClyvzX4bhibWNNB4nDI9WY4qbMGyYyJzrUMjb4xa-6hf0EbOXUh7ePYjKwFyuJJa-tXTj-L_pF_QlyycKF66WPIAl6-soMFIhmZXWlGCUqXHF9-pUkBqlpLLCwz07Z4nUzksMPkoIbHwWf_MUNll-lbaCNgd-z_X97ADu6lTBfhm3bV2eXnDBV5oBoxjF0Fg4fJxl_dNBzw926lswaWwndElr3VyDtpxV5hcsVKFFufunIn6r4leP6K_fYnT6E449IWOaAxYoxyTkmhQDkHBAJ3Odpd19wry1xgi9erRHB059oSr8wP3JqcPqP6hRTdpIEA9id03g'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", dest = 'url', type = str, help="setup URL", required=True)
+    global FLAGS
+    FLAGS, unparsed = parser.parse_known_args()
+    dkubeURL = FLAGS.url
+    authToken = os.getenv('DKUBE_USER_ACCESS_TOKEN')
 
     img, lbl = read_idx(path = inp_path)
-    dataset = pd.DataFrame(data = img.reshape(60000, 784))/255
+    dataset = pd.DataFrame(data = img.reshape(img.shape[0], 784))/255
     dataset['label'] = lbl
     featureset = DkubeFeatureSet()
     featureset.update_features_path(path=out_path)
@@ -42,5 +45,3 @@ if __name__ == "__main__":
 
     api = DkubeApi(URL=dkubeURL, token=authToken)
     api.commit_features()
-
-
