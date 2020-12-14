58bfb2538d6c71eade5fc8cc1fbed1d791bdc461
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-12-14 22:24:32 +0530

    titanic demo

--- sklearn/titanic/program/training.py
+++ sklearn/titanic/program/training.py
@@ -1,13 +1,13 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
-import os
 import numpy as np
-import requests
+import requests, os
 import argparse
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+import mlflow
 
 # import sys
 # sys.path.insert(0, os.path.abspath("/usr/local/lib/python3.6/dist-packages"))
@@ -15,21 +15,7 @@ from dkube.sdk import *
 
 inp_path = "/opt/dkube/input/"
 out_path = "/opt/dkube/output/"
-# filename = 'featureset.parquet'
-
-
-def log_metrics(key, value):
-    url = "http://dkube-exporter.dkube:9401/mlflow-exporter"
-    train_metrics = {}
-    train_metrics["mode"] = "train"
-    train_metrics["key"] = key
-    train_metrics["value"] = value
-    train_metrics["epoch"] = 1
-    train_metrics["step"] = 1
-    train_metrics["jobid"] = os.getenv("DKUBE_JOB_ID")
-    train_metrics["run_id"] = os.getenv("DKUBE_JOB_UUID")
-    train_metrics["username"] = os.getenv("DKUBE_USER_LOGIN_NAME")
-    requests.post(url, json=train_metrics)
+test_path = "/opt/dkube/test"
 
 
 if __name__ == "__main__":
@@ -61,10 +47,19 @@ if __name__ == "__main__":
     # Calculating accuracy
     accuracy = accuracy_score(y, predictions)
     # logging acuracy to DKube
-    log_metrics("accuracy", accuracy)
+    mlflow.log_metric("accuracy", accuracy)
 
     ########--- Write model to DKube ---########
 
     # Exporting model
     filename = os.path.join(out_path, "model.joblib")
     joblib.dump(model_RFC, filename)
+
+    # Writing test data
+    featureset = DkubeFeatureSet()
+    # Specify featureset path
+    featureset.update_features_path(path=test_path)
+    # Read features
+    data = featureset.read()  # output: response json with data
+    test_df = data["data"]
+    test_df.to_csv(os.path.join(out_path, "test.csv"))
