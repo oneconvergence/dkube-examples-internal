2f15bb5ae26f52bc25ea1d6752e1d6654af071c7
riteshkarvaloc <ritesh.k@oneconvergence.com>
2021-02-10 10:52:50 +0530

    transformer.R update

--- R/classification/mnist/transformer/transformer.R
+++ R/classification/mnist/transformer/transformer.R
@@ -2,10 +2,10 @@ library(png)
 library(base64enc)
 library(readr)
 function(base64string){
-    outconn <- file("inp.png","wb")
+    outconn <- file("/tmp/inp.png","wb")
     base64decode(what=base64string, output=outconn)
     close(outconn)
-    x <- readPNG("inp.png")
+    x <- readPNG("/tmp/inp.png")
     y <- array(x, c(1,784))
     y
 }
